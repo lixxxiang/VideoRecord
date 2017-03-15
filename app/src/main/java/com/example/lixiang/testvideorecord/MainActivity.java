25c75fb18604fdb7b6694f95bc722a0796c6837d
lixxxiang <13051744716@163.com>
2017-03-15 15:10:21 +0800

    videorecord

---
+++ app/src/main/java/com/example/lixiang/testvideorecord/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.lixiang.testvideorecord;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.io.File;
+
+import android.view.Window;
+import android.view.WindowManager;
+import mabeijianxi.camera.MediaRecorderActivity;
+import mabeijianxi.camera.VCamera;
+import mabeijianxi.camera.model.MediaRecorderConfig;
+import mabeijianxi.camera.util.DeviceUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //全屏
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN , WindowManager.LayoutParams. FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        initSmallVideo(this);
+    }
+
+    public void go(View c) {
+        MediaRecorderConfig config = new MediaRecorderConfig.Buidler()
+                .doH264Compress(true)
+                .smallVideoWidth(480)
+                .smallVideoHeight(360)
+                .recordTimeMax(6 * 1000)
+                .maxFrameRate(20)
+                .minFrameRate(8)
+                .captureThumbnailsTime(1)
+                .recordTimeMin((int) (1.5 * 1000))
+                .build();
+        MediaRecorderActivity.goSmallVideoRecorder(this, SendSmallVideoActivity.class.getName(), config);
+    }
+
+    public static void initSmallVideo(Context context) {
+        // 设置拍摄视频缓存路径
+        File dcim = Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        if (DeviceUtils.isZte()) {
+            if (dcim.exists()) {
+                VCamera.setVideoCachePath(dcim + "/mabeijianxi/");
+            } else {
+                VCamera.setVideoCachePath(dcim.getPath().replace("/sdcard/",
+                        "/sdcard-ext/")
+                        + "/mabeijianxi/");
+            }
+        } else {
+            VCamera.setVideoCachePath(dcim + "/mabeijianxi/");
+        }
+        // 开启log输出,ffmpeg输出到logcat
+        VCamera.setDebugMode(true);
+        // 初始化拍摄SDK，必须
+        VCamera.initialize(context);
+    }
+}
